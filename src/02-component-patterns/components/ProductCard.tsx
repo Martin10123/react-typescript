bf9bb9e07b390ba33a4ed65c88a893c50aaef5f7
Martin Elias <martinsimarra4@gmail.com>
2022-04-11 10:16:05 -0500

    new seccion

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,5 +1,5 @@
 import { createContext } from 'react';
-import { ProductContextProps, Product } from '../interfaces/interfaces';
+import { ProductContextProps, Product, OnChangeArgs } from '../interfaces/interfaces';
 
 import styles from '../styles/styles.module.css'
 import useProduct from '../hooks/useProduct';
@@ -12,11 +12,13 @@ export interface Props {
     children?: React.ReactElement | React.ReactElement[]
     className?: string
     style?: React.CSSProperties
+    onChange?: (args: OnChangeArgs) => void
+    value?: number
 }
 
-export const ProductCard = ({ children, product, className, style }: Props) => {
+export const ProductCard = ({ children, product, className, style, onChange, value }: Props) => {
 
-    const { increaseBy, counter } = useProduct()
+    const { increaseBy, counter } = useProduct({ onChange, product, value })
 
     return (
         <Provider value={{ counter, increaseBy, product }}>
--- src/02-component-patterns/pages/ShoopingPages.tsx
+++ src/02-component-patterns/pages/ShoopingPages.tsx
@@ -1,38 +1,55 @@
+
 import { ProductButtons, ProductTitle, ProductCard, ProductImage } from '../components';
+import { products } from '../data/products';
+import { useShoopingCart } from '../hooks/useShoopingCart';
+
 
 import "../styles/custom-styles.css";
 
-const product = {
-    id: "1",
-    title: "coffee - muggi",
-    img: "./coffee-mug.png"
-}
+
 
 const ShoopingPages = () => {
+
+    const { shoppingCart, onProductChange } = useShoopingCart()
+
     return (
         <div>
             <h1>Shooping Pages</h1>
             <hr />
             <div style={{ display: "flex", flexWrap: "wrap" }}>
-                <ProductCard product={product} >
-                    <ProductCard.Image />
-                    <ProductCard.Title title={"Cafe"} />
-                    <ProductCard.Buttons />
-                </ProductCard>
+                {products.map(product => (
+                    <ProductCard
+                        key={product.id}
+                        product={product}
+                        className="bg-dark"
+                        value={shoppingCart[product.id]?.count || 0}
+                        onChange={(evento) => onProductChange(evento)}
+                    >
+                        <ProductImage className="custom-image" />
+                        <ProductTitle className="text-white" />
+                        <ProductButtons className="custom-buttons" />
+                    </ProductCard>
 
-                <ProductCard product={product} className="bg-dark" >
-                    <ProductImage className="custom-image" />
-                    <ProductTitle className="text-white" />
-                    <ProductButtons className="custom-buttons" />
-                </ProductCard>
-
-                <ProductCard product={product}>
-                    <ProductImage />
-                    <ProductTitle />
-                    <ProductButtons />
-                </ProductCard>
+                ))}
             </div>
-        </div>
+
+            <div className="custom-cart">
+
+                {Object.entries(shoppingCart).map(([key, product]) => (
+                    <ProductCard
+                        key={key}
+                        product={product}
+                        className="bg-dark"
+                        style={{ width: "150px" }}
+                        value={product.count}
+                        onChange={onProductChange}
+                    >
+                        <ProductImage className="custom-image" />
+                        <ProductButtons className="custom-buttons" />
+                    </ProductCard>
+                ))}
+            </div>
+        </div >
     )
 }
 
